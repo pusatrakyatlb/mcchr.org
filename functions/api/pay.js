aad13080b61e1b94da26060f57435fbb87d94b7c
MCCHR <github.com@loyarburok.com>
2023-01-06 04:29:02 +0800

    Create pay.js

---
+++ functions/api/pay.js
@@ -0,0 +1,8 @@
+https://mcchr.org/pay?mode=subscription
+                     &cancel=https%3A%2F%2Fsupport.mcchr.org%2F
+                     &success=https%3A%2F%2Fsupport.mcchr.org%2Fthankyou%2F
+                     &currency=myr
+                     &email=joe@bloggs.com
+                     &products=1|semiannualdonationtomcchr_4_6month_10000
+                     &items=2|MCCHR XII Stickers|12.12
+                     &items=1|Delivery within Malaysia|1.50
