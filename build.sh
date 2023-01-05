9a203348da1a7b2c089d8e908ab3b5a6b1999131
MCCHR <github.com@loyarburok.com>
2023-01-06 05:21:57 +0800

    stripe-checkout

---
+++ build.sh
@@ -0,0 +1,6 @@
+### Cloudflare Pages
+### Don't forget to set the `Build command` field value to `bash build.sh` in the project settings
+
+hugo # Hugo Jamstack framework
+
+npm i stripe # Stripe Checkout via /functions/api/pay.js
